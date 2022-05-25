2e56b9945f4ae1270d02c701a6e6db0881e483a0
Julio Acevedo <61912523+julio-x-a@users.noreply.github.com>
2022-05-24 22:18:39 -0500

    cambios para testing

--- main.js
+++ main.js
@@ -1,52 +1,52 @@
 const d = document;
 const list = d.getElementById('list');
 
-// window.onload = () => {
-//   detect();
-// };
+window.onload = () => {
+  detect();
+};
 
-// async function detect() {
-//   const barcodeDetector = new BarcodeDetector();
-//   const list = document.getElementById('barcode-list');
-//   let itemsFound = [];
-//   const mediaStream = await navigator.mediaDevices.getUserMedia({
-//     video: { facingMode: 'environment' },
-//   });
-
-//   const video = document.createElement('video');
-//   video.srcObject = mediaStream;
-//   video.autoplay = true;
-
-//   list.before(video);
-
-//   function render() {
-//     barcodeDetector
-//       .detect(video)
-//       .then((barcodes) => {
-//         barcodes.forEach((barcode) => {
-//           if (!itemsFound.includes(barcode.rawValue)) {
-//             itemsFound.push(barcode.rawValue);
-//             const li = document.createElement('li');
-//             li.innerHTML = barcode.rawValue;
-//             list.appendChild(li);
-//           }
-//         });
-//       })
-//       .catch(console.error);
-//   }
-
-//   (function renderLoop() {
-//     requestAnimationFrame(renderLoop);
-//     render();
-//   })();
-// }
-
-d.addEventListener('DOMContentLoaded', async (e) => {
+async function detect() {
+  const barcodeDetector = new BarcodeDetector();
+  const list = document.getElementById('barcode-list');
+  let itemsFound = [];
   const mediaStream = await navigator.mediaDevices.getUserMedia({
     video: { facingMode: 'environment' },
   });
+
   const video = document.createElement('video');
   video.srcObject = mediaStream;
   video.autoplay = true;
+
   list.before(video);
+
+  function render() {
+    barcodeDetector
+      .detect(video)
+      .then((barcodes) => {
+        barcodes.forEach((barcode) => {
+          if (!itemsFound.includes(barcode.rawValue)) {
+            itemsFound.push(barcode.rawValue);
+            const li = document.createElement('li');
+            li.innerHTML = barcode.rawValue;
+            list.appendChild(li);
+          }
+        });
+      })
+      .catch(console.error);
+  }
+
+  (function renderLoop() {
+    requestAnimationFrame(renderLoop);
+    render();
+  })();
+}
+
+// d.addEventListener('DOMContentLoaded', async (e) => {
+//   const mediaStream = await navigator.mediaDevices.getUserMedia({
+//     video: { facingMode: 'environment' },
+//   });
+//   const video = document.createElement('video');
+//   video.srcObject = mediaStream;
+//   video.autoplay = true;
+//   list.before(video);
 });
